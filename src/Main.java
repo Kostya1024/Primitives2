37cb8c7b200680739c0a59876683f27fb549d5d6
Kostya <snaaack@.mail.ru>
2022-06-12 05:11:16 +0200

    code v2

--- src/Main.java
+++ src/Main.java
@@ -8,18 +8,17 @@ public class Main {
         double amountOnAccount; // Сумма на счету
 
         startingScore = 5;
-        replenishmentAmount = 2_000.10;
-
+        replenishmentAmount = 1199.10;
 
 
         if (replenishmentAmount > 1000) {
             long bonus = (long) replenishmentAmount / 100; // Количество начисленных бонусов
-            amountOnAccount = startingScore + replenishmentAmount + (double) bonus;
+            amountOnAccount = startingScore + replenishmentAmount + bonus;
             System.out.println("На Ваш счет поступил платеж в размере: " + replenishmentAmount + " рублей. Ваш бонус от пополнения составляет: " + bonus + " рублей. Баланс Вашего счета: " + amountOnAccount + " рублей");
         } else {
             long bonus = 0; // Количество начисленных бонусов
             amountOnAccount = startingScore + replenishmentAmount;
-            System.out.println("На Ваш счет поступил платеж в размере: " + replenishmentAmount + " рублей. Ваш бонус от пополнения составляет: " + bonus + " рублей. Баланс Вашего счета: " + amountOnAccount + " рублей");
+            System.out.println("На Ваш счет поступил платеж в размере: " + replenishmentAmount + " рублей. Ваш бонус от пополнения составляет: " + bonus + " рублей (для получения бонусов следует пополнить счет более чем на 1000 рублей). Баланс Вашего счета: " + amountOnAccount + " рублей");
 
         }
     }
